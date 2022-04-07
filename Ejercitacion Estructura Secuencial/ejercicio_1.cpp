ca2c36db19f980df5dd1adb33f0f5b29e180a026
Eze Gatica <ezequielgatica@gmail.com>
2022-04-07 20:16:47 -0300

    Ejercicio 1

---
+++ Ejercitacion Estructura Secuencial/ejercicio_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, suma;
+    cout << "Ingrese un número" << endl;
+    cin >> num1;
+    cout << "Ingrese otro número" << endl;
+    cin >> num2;
+    suma = num1 + num2;
+    cout << "La suma es: " << suma << endl;
+    
+    return 0; // Para que no rompa
+}
