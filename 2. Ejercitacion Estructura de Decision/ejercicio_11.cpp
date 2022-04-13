0d09291acd7dd9f6b40a3d8eda281b87a0b6edf9
Eze Gatica <ezequielgatica@gmail.com>
2022-04-13 12:18:02 -0300

    Ejercicio 11 terminado

--- 2. Ejercitacion Estructura de Decision/ejercicio_11.cpp
+++ 2. Ejercitacion Estructura de Decision/ejercicio_11.cpp
@@ -10,9 +10,55 @@ descuento del 10% sobre el total.
 .código 4; 44: 2 peso la unidad.
 Confeccionar un programa que ingrese como dato el código de un artículo y la cantidad a
 comprar y se informe el importe de la compra:
- ARTÍCULO xxxxx CANTIDAD xxxx IMPORTE A PAGAR $ xxxx.xx 
+ ARTÍCULO xxxxx CANTIDAD xxxx IMPORTE A PAGAR $ xxxx.xx
  */
-int main() {
-    
+
+// FORMATEAR LA RESPUESTA COMO PIDE
+
+int main()
+{
+    int codigo, cantidad;
+    cout << "Ingrese un numero de articulo: ";
+    cin >> codigo;
+    cout << "Ingrese la cantidad de articulos a comprar: ";
+    cin >> cantidad;
+    switch (codigo)
+    {
+    case 1:
+    case 10:
+    case 100:
+        cout << "Valor: $" << 10 * cantidad << endl;
+        break;
+    case 2:
+    case 22:
+    case 222:
+        if (cantidad % 10 == 0)
+        {
+            int cantidadDeCajas = cantidad / 10;
+            cout << "Valor: $" << 65 * cantidadDeCajas << endl;
+        }
+        else
+        {
+            cout << "Valor: $" << 7 * cantidad << endl;
+        }
+        break;
+    case 3:
+    case 33:
+        if (cantidad > 10)
+        {
+            cout << "Valor: $" << ((3 * cantidad) * 0.9) << endl;
+        }
+        else
+        {
+            cout << "Valor: $" << 3 * cantidad << endl;
+        }
+        break;
+    case 4:
+    case 44:
+        cout << "Valor: $" << 2 * cantidad << endl;
+        break;
+    default:
+        break;
+    }
     return 0;
 }
