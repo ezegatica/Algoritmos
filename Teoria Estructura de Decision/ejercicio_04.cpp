3b96ed1a226b5234d9ceb532d70e617293fa748c
Eze Gatica <ezequielgatica@gmail.com>
2022-04-13 09:55:06 -0300

    Ej4 y para pensar un ratito

---
+++ Teoria Estructura de Decision/ejercicio_04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+/*
+    Confeccionar un programa que solicite dos valores reales positivos, expresados en cm. El
+    primero corresponde a la base y el segundo a la altura de un triángulo, se desea calcular el
+    valor de la superficie. Si la superficie calculada es menor a 100 cm2, expresarla en milímetros cuadrados, 
+    caso contrario en metros cuadrados. 
+    Informar resultado con unidades. (1 m2 = 10.000 cm2)
+*/
+int main() {
+    int num1, num2;
+    
+    return 0;
+}
