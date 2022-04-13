f80f3267288669085f38a22ad17822baae1778d7
Eze Gatica <ezequielgatica@gmail.com>
2022-04-13 09:01:54 -0300

    Usando constantes en vez de variables

--- Ejercitacion Estructura Secuencial/ejercicio_7.cpp
+++ Ejercitacion Estructura Secuencial/ejercicio_7.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#define PI 3.14
 
 using namespace std;
 // 7. Dado el radio de un círculo, informar el diámetro, el perímetro y la superficie del mismo. 
 
 int main() {
     int radio, diametro, perimetro, superficie;
-    float pi = 3.1415f;
+    // const float PI = 3.14;
     cout << "Ingresar el radio del círculo: ";
     cin >> radio;
     diametro = radio + radio;
-    perimetro = 2 * pi * radio;
-    superficie = pi * radio * radio;
+    perimetro = 2 * PI * radio;
+    superficie = PI * radio * radio;
     cout << "El diametro del circulo es: " << diametro << endl;
     cout << "El perimetro del circulo es: " << perimetro << "cm" << endl;
     cout << "La superficie del circulo es: " << superficie << "cm²" << endl;
+    
     return 0;
 }
