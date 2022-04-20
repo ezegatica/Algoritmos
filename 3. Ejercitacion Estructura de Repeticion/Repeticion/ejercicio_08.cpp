444071d96b1d64c78f6afc8f9998cb59479b2e49
Eze Gatica <ezequielgatica@gmail.com>
2022-04-20 19:43:14 -0300

    Cambiso varios

---
+++ 3. Ejercitacion Estructura de Repeticion/Repeticion/ejercicio_08.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+/*
+8) Dado un número entero positivo entre 1 y 3999 informar su correspondiente número Romano.
+*/
+int main() {
+    int n;
+    cout << "Ingrese un número entero positivo entre 1 y 3999: ";
+    cin >> n;
+    
+    return 0;
+}
