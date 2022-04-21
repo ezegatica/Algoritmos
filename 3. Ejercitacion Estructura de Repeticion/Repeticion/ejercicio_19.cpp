4704db9e036b31917575a72a6bb50a0d5297272e
Eze Gatica <ezequielgatica@gmail.com>
2022-04-20 21:12:05 -0300

    Ejercicio 19

--- 3. Ejercitacion Estructura de Repeticion/Repeticion/ejercicio_19.cpp
+++ 3. Ejercitacion Estructura de Repeticion/Repeticion/ejercicio_19.cpp
@@ -9,6 +9,32 @@ cantidad de cuentas con saldo deudor, con saldo acreedor y con saldo nulo. Para 
 ingresar un número de cuenta negativo.
 */
 int main() {
-    
+    int acreedor = 0;
+    int deudor = 0;
+    int nulo = 0;
+    while(true){
+        int cuenta;
+        string nombre;
+        int saldo;
+        cout << "Ingrese el numero de cuenta: ";
+        cin >> cuenta;
+        if (cuenta < 0) {
+            break;
+        }
+        cout << "Ingrese el nombre del cliente: ";
+        cin >> nombre;
+        cout << "Ingrese el saldo: ";
+        cin >> saldo;
+        if (saldo < 0) {
+            deudor++;
+        } else if (saldo > 0) {
+            acreedor++;
+        } else {
+            nulo++;
+        }
+    }
+    cout << "La cantidad de cuentas con saldo deudor es: " << deudor << endl;
+    cout << "La cantidad de cuentas con saldo acreedor es: " << acreedor << endl;
+    cout << "La cantidad de cuentas con saldo nulo es: " << nulo << endl;
     return 0;
 }
